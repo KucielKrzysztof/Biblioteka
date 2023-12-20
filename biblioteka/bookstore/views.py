bf91e66ce760ce2bf50537c4057b13477fb0081a
KucielKrzysztof <s223871@student.uek.krakow.pl>
2023-12-20 17:29:25 +0100

    add product in cart total

--- biblioteka/bookstore/views.py
+++ biblioteka/bookstore/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from .models import *
 
+
 def store(request):
      products = Product.objects.all()
-     context = {'products':products} #Tworzy się słownik context, który zawiera dane, jakie zostaną przekazane do szablonu Django (store.html). Tutaj, 'products' jest kluczem w słowniku, a wartością jest lista produktów z bazy danych.
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False) # Pobranie lub utworzenie niezakończonego zamówienia dla tego klienta
+          items = order.orderitem_set.all() #Pobranie wszystkich elementów zamówienia (OrderItem) dla danego zamówienia (Order)
+     else:
+          items = []
+          order = {'get_cart_items':0, }
+     
+     context = {'products':products, 'items': items, 'order':order} #Tworzy się słownik context, który zawiera dane, jakie zostaną przekazane do szablonu Django (store.html). Tutaj, 'products' jest kluczem w słowniku, a wartością jest lista produktów z bazy danych.
      return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -27,3 +36,4 @@ def checkout(request):
           order = {'get_cart_items':0, }
      context = {'items': items, 'order':order}
      return render(request, 'store/checkout.html', context)
+
