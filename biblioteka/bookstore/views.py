294ecfbf60cf6e57005d2cc3bac00c45fb5e42b6
KucielKrzysztof <s223871@student.uek.krakow.pl>
2023-12-21 17:47:09 +0100

    dodaje funkcjionalność: dodawanie książek do koszyka

--- biblioteka/bookstore/views.py
+++ biblioteka/bookstore/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
 
 
 def store(request):
@@ -37,3 +39,26 @@ def checkout(request):
      context = {'items': items, 'order':order}
      return render(request, 'store/checkout.html', context)
 
+def updateItem(request):
+     data = json.loads(request.body) # Pobiera dane z ciała żądania
+     productId = data['productId']
+     action = data['action'] # Pobiera akcję (np. 'add' lub 'remove') z danych żądania
+     print('Action:', action)
+     print('ProductID:', productId)
+
+     customer = request.user.customer
+     product = Product.objects.get(id=productId)
+     order, created = Order.objects.get_or_create(customer=customer, complete=False)  # Pobranie lub utworzenie elementu zamówienia dla określonego produktu i zamówienia
+     orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+     if action == 'add':
+          orderItem.quantity = (orderItem.quantity + 1)
+     elif action == 'remove':
+          orderItem.quantity = (orderItem.quantity - 1)
+
+     orderItem.save()  # Zapisanie zmian ilości produktu w elemencie zamówienia
+
+     if orderItem.quantity <= 0:
+          orderItem.delete()
+
+     return JsonResponse('Item was added', safe=False)
